cec3e0aae0cb8ce962e77c6eac0e4958bd2a83a4
meow-error <egorlevadniy2002@gmail.com>
2022-06-17 13:42:12 +0300

    add order-window GUI Добавлен интерфейс для добавления заказа

--- src/main/java/com/example/pizza/OrderWindowController.java
+++ src/main/java/com/example/pizza/OrderWindowController.java
@@ -1,9 +1,52 @@
 package com.example.pizza;
 
-import com.example.pizza.product.Product;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 public class OrderWindowController {
+
+    @FXML
+    private ChoiceBox<?> firstOptBox;
+
+    @FXML
+    private ChoiceBox<?> fourOptBox;
+
+    @FXML
+    private ListView<?> listView;
+
+    @FXML
+    private Button minusBut;
+
+    @FXML
+    private TextField numberField;
+
+    @FXML
+    private Button plusBut;
+
+    @FXML
+    private ChoiceBox<?> productBox;
+
+    @FXML
+    private ChoiceBox<?> secondOptBox;
+
+    @FXML
+    private ChoiceBox<?> thirdOptBox;
+
+    @FXML
+    void addProduct(ActionEvent event) {
+
+    }
+
+    @FXML
+    void addOrder(ActionEvent event) {
+
+        Stage stage = (Stage) plusBut.getScene().getWindow();
+        stage.close();
+    }
 
 }
